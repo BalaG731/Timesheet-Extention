bc3c22e0c7d99ccf68e9a7f8eeb3c1b3c3bcf96f
BalaG731 <balaparamasivambd@gmail.com>
2018-10-05 12:53:09 +0530

    WFH data and Half day leaves are calculated in this commit.

--- Timesheet/Timesheet/content.js
+++ Timesheet/Timesheet/content.js
@@ -2,13 +2,17 @@ var appvision_ext;
 setTimeout(function() {    
     var values = [];
     var duplicate = [];
+    var wfhVal = [];
+    var WFH = [];
+    var Leave = [];
+    var AD = [];
+    var HLeave = [];
     var bgColor;
     var hoursLabel;
     var laggedHours;
 
     //Swip in days count will be stored in values array.
     $(".grdwfa").find("tr").each(function() { values.push($(this).find("td:nth-child(2)").html()); })
-
     //leave days count will be stored in duplicate array.
     for (var i = 0; i < values.length; i++) { 
         var duplicateContent = "&nbsp;"; 
@@ -16,31 +20,63 @@ setTimeout(function() {
         currentvalue = values[i]; 
         currentvalue === duplicateContent ? duplicate.push(currentvalue) : null; }
 
-    var presentDays = values.length - duplicate.length - 1;
+    //WFH days count will be stored in wfhVal array.
+    $(".grdwfa").find("tr").each(function() { wfhVal.push($(this).find("td:nth-child(7)").html()); })
+    //WFH days count will be stored in WFH array.
+    for (var i = 0; i < wfhVal.length; i++) { 
+        var wfhCurrentvalue = "";
+        wfhCurrentvalue = wfhVal[i]; 
+        if(wfhCurrentvalue === "WFH"){
+            WFH.push(wfhCurrentvalue);
+        }        
+        else if (wfhCurrentvalue === "Leave") {
+            Leave.push(wfhCurrentvalue);
+        }
+        else if (wfhCurrentvalue === "Auto-Deduct") {
+            AD.push(wfhCurrentvalue);
+        }
+        else if (wfhCurrentvalue === "Leave(H)") {
+            HLeave.push(wfhCurrentvalue);
+        }
+    }
+
+    var wfhDays = WFH.length;
+    var Leave = Leave.length;
+    var AD = AD.length;
+    var HLeave = HLeave.length / 2 ;
+    var presented = values.length;
+
+    var presentDays = presented - duplicate.length - 1;
+        presentDays = presentDays + wfhDays - HLeave;
 
     var totalHours = document.getElementById('MainContent_lblTotalHours').textContent;
     totalHours = totalHours.split(" ")[0];
 
     var totalDays = document.getElementById('MainContent_lblWorkingDays').textContent;
     totalDays = totalDays.split(" ")[0];
-
-    var leaveDays = document.getElementById('MainContent_lblLeaveDays').textContent;
-    leaveDays = leaveDays.split(" ")[0];
-
-        
+    
+    
+    // var leaveDays = document.getElementById('MainContent_lblLeaveDays').textContent;
+    // leaveDays = leaveDays.split(" ")[0];
+    debugger;
+    var leaveDays = Leave + HLeave + AD;
     var requiredHours = (totalDays - leaveDays) * 8;
+
+    var requiredPresentHours = presentDays * 8;
 
     var sortedHours = requiredHours - totalHours;
 
     var num = sortedHours / (totalDays - presentDays);
 
+    //var num = sortedHours;
+
     //var metExpectedHours = sortedHours / num;
     var metExpectedHours = ('0' + Math.floor(num) % 24).slice(-2) + ':' + ((num % 1) * 60 + '0').slice(0, 2); 
 
     sortedHours >= 0 ? bgColor = "crimson" : bgColor = "green";
-    sortedHours >= 0 ? hoursLabel = "Total sorted hours for this month: " : hoursLabel = "Above expected hours for this month: ";
+    sortedHours >= 0 ? hoursLabel = "Lack hours for this month*: " : hoursLabel = "Above expected hours for this month: ";
 
-    var style = `background:${bgColor}; color: #fff; padding: 5px;`;
+    var style = `background: none; color: ${bgColor}; padding: 5px; font-weight: bold;`;
     
     //get current month 
     var month = new Array();
@@ -81,14 +117,14 @@ setTimeout(function() {
     var requiredHoursStyles;
     var requiredHoursBG;
     spliceHours<8 ? requiredHoursBG = "crimson" : requiredHoursBG= "green";
-    var requiredHoursStyles = `background:${requiredHoursBG}; color: #fff; padding: 5px;`;
+    var requiredHoursStyles = `background: none; color: ${requiredHoursBG}; padding: 5px; font-weight: bold;`;
 
     //Till now sorted hours for presented days
     var presentedDaysTotalHours = presentDays*8;
     var  presentedSortHours = totalHours - presentedDaysTotalHours;
     var presentedSortHoursBG;
     presentedSortHours < 0 ? presentedSortHoursBG="crimson" : presentedSortHoursBG = "green";
-    var presentedSortHoursStyles = `background:${presentedSortHoursBG}; color: #fff; padding: 5px;`;
+    var presentedSortHoursStyles = `background: none; color: ${presentedSortHoursBG}; padding: 5px; font-weight: bold;`;
     var presentedSortHoursText;
     presentedSortHours < 0 ? presentedSortHoursText="lagged" : presentedSortHoursText="overtime";
 
@@ -100,6 +136,16 @@ setTimeout(function() {
     //console.log(lastOutTime);
     console.groupEnd();
 
-    $("#header").append("<div style='right: 0; background: #f4f4f4; color: #333; padding: 10px;font-size: 1em; box-shadow:#d2d2d2 0px 3px 1px; line-height:30px;'> <div style='margin-bottom:5px; padding-left: 15px; display: inline;" + requiredHoursStyles + "'>For today you need  : " + neededHours + " hrs to match.</div> <div style='margin-bottom:5px; padding-left: 5px; display: inline; margin-left:3px;" + presentedSortHoursStyles + "'>Total present days:" + presentDays + ", Till EOD :"+ presentedSortHours +" hours "+ presentedSortHoursText +".</div> <div style='margin-bottom:5px; padding-left: 5px; display: inline;'>Total expected hours for this month : " + requiredHours + " hrs.</div> <div style='margin-bottom:5px; padding-left: 5px; display: inline;'>Total swiped : " + totalHours + " hrs</div> <div style ='display: inline;padding-left: 15px;" + style + "'> " + hoursLabel + sortedHours + " hrs </div> <div style='margin:12px 0 0 0; padding-left: 5px;display: inline;'>From tomorrow onwords you need to log: " + laggHours + "hrs only.</div></div>")
-    
+    $("#header").append("\
+        <div style='right: 0; background: #fff; color: #333; padding: 10px 10px 0 10px;font-size: 1em; line-height:30px;'> \
+        <div style='margin-bottom:5px; padding-left: 5px; display: inline;'>Total number of days present:" + presentDays + ".</div> \
+        <div style='margin-bottom:5px; padding-left: 5px; display: inline;'> Expected hours for this month : " + requiredHours + " hrs.</div> \
+        <div style='margin-bottom:5px; padding-left: 5px; display: inline;'></div> \
+        <div style='margin-bottom:5px; padding-left: 5px; display: inline;'>Swiped hours for this month* : " + totalHours + " hrs.</div> \
+        <div style ='display: inline;padding-left: 15px;" + style + "'>" + hoursLabel + sortedHours + " hrs.</div> \
+        <div style='margin-bottom:5px; padding-left: 15px; display: inline;" + requiredHoursStyles + "'>Expected hours for today*  : " + neededHours + " hrs to match.</div> \
+        <div style='margin-bottom:5px; padding-left: 5px; display: inline;" + presentedSortHoursStyles + "'>Deviation until today* : " + presentedSortHours +" hours "+ presentedSortHoursText +".</div> \
+        <div style='margin:12px 0 0 0; padding-left: 5px;display: inline;'>Expected swipe hours from tomorrow: " + laggHours + "hrs only.</div> \
+        <div style='padding-left: 5px;background: none;font-style: italic;text-align: right;font-size: 11px;'>note: * is based on your last swipe out</div>\
+        </div>")    
 }, 500);
